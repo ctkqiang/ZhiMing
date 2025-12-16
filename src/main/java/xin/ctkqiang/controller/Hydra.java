033fc780a5894b1cda9703ee66a6adf4f6fc8473
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2025-12-16 23:54:07 +0800

    feat(Hydra): 添加密码字段并实现命令执行功能
    添加password字段及其getter/setter方法
    实现hydra命令执行逻辑，包括输出处理和错误捕获

--- src/main/java/xin/ctkqiang/controller/Hydra.java
+++ src/main/java/xin/ctkqiang/controller/Hydra.java
@@ -1,5 +1,9 @@
 package xin.ctkqiang.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import xin.ctkqiang.interfaces.HydraInterface;
 import xin.ctkqiang.utilities.Logger;
 
@@ -14,7 +18,7 @@ public class Hydra implements HydraInterface{
     public String F;
 
     public String user;
-
+    public String password;
 
     public Hydra() { }
 
@@ -28,6 +32,10 @@ public class Hydra implements HydraInterface{
 
     public String getUser() {
         return user;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getPasswordFile() {
@@ -66,6 +74,10 @@ public class Hydra implements HydraInterface{
         this.user = user;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public void onInit() {
         logger.info("Hydra 模块初始化完成");
@@ -80,7 +92,7 @@ public class Hydra implements HydraInterface{
     public void bruteForceHttpPostForm(String url, int port, int threads, int timeout) {
         logger.info("开始进行 HTTP POST 暴力破解");
 
-        String cmd; 
+        String cmd, line; 
 
         // hydra -L user.txt  -P rockyou.txt -f -t 1 -s 6022 -W 100000 -w 75  -vV 'http-post-form://urllogin:email=^USER^&password=^PASS^:F=用户名或密码错误'
         if (providePasswordFile && provideUserFile) {
@@ -90,5 +102,28 @@ public class Hydra implements HydraInterface{
         else if (providePasswordFile && !provideUserFile) {
             cmd = "hydra -l " + getUser() + "-P " + filePathPassword + " -f -t " + threads + " -s " + port + " -W 100000 -w 75  -vV 'http-post-form://" + url + ":email=^USER^&password=^PASS^:F=用户名或密码错误'";
         } 
+
+        else if (!providePasswordFile && provideUserFile) {
+            cmd = "hydra -u -L " + filePathUser + " -p " + getPassword() + " -f -t " + threads + " -s " + port + " -W 100000 -w 75  -vV 'http-post-form://" + url + ":email=^USER^&password=^PASS^:F=用户名或密码错误'";
+        }
+
+        else {
+            cmd = "hydra --version";
+        }
+
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            
+            logger.info("Hydra 输出");
+
+            while ((line = reader.readLine()) != null) System.out.println(line);
+            int exitCode = process.waitFor();
+
+            logger.info("Hydra 执行完成，退出码: " + exitCode);
+
+        } catch (IOException  | InterruptedException e) {
+            logger.error("执行 Hydra 命令时出错：{}", e.getMessage());
+        }
     }
 }
